1fd743aeb243e005301aa291d20b708422428d8c
Zahra-Dehghan98 <zahradn9877@gmail.com>
2026-03-08 22:05:29 +0330

    customize models in onlineshop app:cartitem, order, orderitem

--- onlineshop/models.py
+++ onlineshop/models.py
@@ -2,7 +2,7 @@ from django.contrib.auth.models import User
 from django.db import models
 
 class CustomerProfile(models.Model):
-    user = models.OneToOneField(User, on_delete = models.CASCADE, verbose_name='مشتری')
+    user = models.OneToOneField(User, on_delete = models.CASCADE, related_name='customerprof', verbose_name='مشتری')
     phone = models.CharField(max_length=11, unique=True, blank=False, null=False, verbose_name='شماره تماس', editable=True)
     balance = models.PositiveIntegerField(default=0, verbose_name='موجودی کیف پول' ,editable=True)
 
@@ -15,7 +15,7 @@ class CustomerProfile(models.Model):
         return self.user.username
     
 class SellerProfile(models.Model):
-    user = models.OneToOneField(User, on_delete = models.CASCADE, verbose_name='فروشنده')
+    user = models.OneToOneField(User, on_delete = models.CASCADE,related_name='sellerprof', verbose_name='فروشنده')
     phone = models.CharField(max_length=11, unique=True, blank=False, null=False, verbose_name='شماره تماس', editable=True)
    
 
@@ -29,38 +29,87 @@ class SellerProfile(models.Model):
     
 
 class Store(models.Model):
-    name = models.CharField(max_length=50, unique=True, blank=False, null=False, verbose_name='فروشگاه')
+    name = models.CharField(max_length=50, unique=True, blank=False, null=False, related_name='store', verbose_name='فروشگاه')
     seller = models.ForeignKey(SellerProfile, on_delete = models.CASCADE, verbose_name='فروشنده')
     description = models.TextField(max_length=300, blank=True, null=True, verbose_name='اطلاعات تماس', editable=True)
     location = models.CharField(max_length=50, blank=True, null=True, verbose_name='مکان', editable=True)
-    rating = models.DecimalField(max_digits=5, decimal_places =1, blank=True, null=True, editable=True)
+    rating = models.DecimalField(max_digits=5, decimal_places =1, blank=True, null=True, editable=True, verbose_name='امتیاز')
     
 
     class Meta:
         verbose_name = 'فروشگاه'
         verbose_name_plural = 'فروشگاه ها'
-        ordering = ['created_at']
+        ordering = ['name']
 
     def __str__(self):
         return self.name
     
 class Product(models.Model):
-    name = models.CharField(max_length=50, unique=True, blank=False, null=False, verbose_name='نام کالا')
-    balance = models.PositiveIntegerField(blank=False, null=False, verbose_name='قیمت', editable=True)
+    name = models.CharField(max_length=50, unique=True, blank=False, null=False, related_name='product', verbose_name='نام کالا')
+    price = models.PositiveIntegerField(blank=False, null=False, verbose_name='قیمت', editable=True)
     description = models.TextField(max_length=300, blank=True, null=True, verbose_name='اطلاعات محصول')
     image = models.ImageField(upload_to='productpic/', blank=False, null=False, verbose_name='تصویر محصول', editable=True)
     store = models.ForeignKey(Store, on_delete = models.CASCADE, verbose_name='فروشگاه')
     created_at = models.DateTimeField(auto_now_add=True)
-    rating = models.DecimalField(max_digits=5, decimal_places =1, blank=True, null=True, editable=True)
+    rating = models.DecimalField(max_digits=5, decimal_places =1, blank=True, null=True, editable=True, verbose_name='امتیاز')
     
 
     class Meta:
-        verbose_name = 'فروشگاه'
-        verbose_name_plural = 'فروشگاه ها'
+        verbose_name = 'محصول'
+        verbose_name_plural = 'محصولات'
         ordering = ['created_at']
 
     def __str__(self):
         return self.name
+    
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE, related_name='cartitem', verbose_name='نام محصول')
+    customer = models.ForeignKey(CustomerProfile, on_delete=models.CASCADE, verbose_name='مشتری')
+    quantity = models.PositiveIntegerField(default=1 ,verbose_name='تعداد محصول')
+    
+    class Meta:
+        verbose_name = 'سبد خرید'
+        ordering = ['customer']
+        unique_together = ('customer', 'product')
+
+    def __str__(self):
+        return f'تعداد {self.quantity} - محصول{self.product} در سبد خرید {self.customer}'
+
+class Order(models.Model):
+    class OrderStatus(models.TextChoices):
+        PENDING = 'pending', ('در انتظار')
+        PROCESSING = 'processing', ('در حال پردازش')
+        SHIPPED = 'shipped', ('ارسال شده')
+        DELIVERED = 'delivered', ('تحویل شده')
+        CANCELLED = 'cancelled', ('لغو شده')
+        RETURNED = 'returned', ('مرجوع شده')
+    customer = models.ForeignKey(CustomerProfile, on_delete=models.CASCADE, related_name='order', verbose_name='مشتری')
+    total_amount = models.PositiveIntegerField(verbose_name='مبلغ کل')
+    date = models.DateTimeField(auto_now_add=True, verbose_name='تاریخ سفارش')
+    status = models.CharField(choices=OrderStatus.choices, default=OrderStatus.PENDING, verbose_name='وضعیت سفارش')
+    
+
+    class Meta:
+        verbose_name = 'سفارش'
+        verbose_name_plural = 'سفارش ها'
+        ordering = ['date']
+
+    def __str__(self):
+        return f'سفارش {self.customer}در تاریخ {self.date} به مبلغ {self.total_amount}'
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE, related_name='orderitem', verbose_name='سفارش')
+    product = models.ForeignKey(Product, on_delete=models.SET_NULL, verbose_name='نام محصول')
+    quantity = models.PositiveIntegerField(default=1 ,verbose_name='تعداد محصول')
+    price = models.PositiveIntegerField(verbose_name='قیمت')
+
+
+    class Meta:
+        verbose_name = 'آیتم سفارش'
+        verbose_name_plural = 'آیتم های سفارش'
+        unique_together = ('order', 'product')
+        ordering = ['order']
+
     
 
 
