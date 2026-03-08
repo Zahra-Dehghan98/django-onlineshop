d0ac947dfb340dbe0d2cecbea95d218c7f989e10
Zahra-Dehghan98 <zahradn9877@gmail.com>
2026-03-08 14:03:18 +0330

    Configure static and media file paths

--- config/settings/base.py
+++ config/settings/base.py
@@ -111,3 +111,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
 STATIC_URL = 'static/'
+MEDIA_URL = 'media/'
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
