544fe09b321d87f2994fc8d4f97c949ea206b6dc
Zahra-Dehghan98 <zahradn9877@gmail.com>
2026-03-17 18:25:18 +0330

    customize signup and login form

---
+++ onlineshop/form.py
@@ -0,0 +1,14 @@
+from django import forms
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from .models import *
+
+class UserRegisterForm(UserCreationForm):
+    phone = forms.CharField(max_length=11)
+    is_seller = forms.BooleanField(required=False)
+    class Meta:
+        model = User
+        fields = ['phone', 'first_name', 'last_name', 'password1', 'password2', 'is_seller']
+
+class UserLoginForm(AuthenticationForm):
+    username = models.CharField(max_length=11)
+    password = models.CharField()
